544746eddb02172bd16fa0d4e4c8b12253eae8a6
zdkh <azaryageoffrey@outlook.com>
2023-11-18 20:45:39 +0700

    chore(landpage): base content

---
+++ main.js
@@ -0,0 +1,12 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#element", {
+  strings: [
+    "Civil Engineer.",
+    "3D Artist.",
+    "Self-Taught Programmer.",
+    "Photographer.",
+  ],
+  typeSpeed: 50,
+  loop: true,
+});
