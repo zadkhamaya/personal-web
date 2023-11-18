5a44dc427a385221da7930bbbe5b0935dbe12374
zdkh <azaryageoffrey@outlook.com>
2023-11-19 00:22:28 +0700

    chore(landpage): 0.1.0

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Typed from "typed.js";
+import confetti from "canvas-confetti";
 
 const typed = new Typed("#element", {
   strings: [
@@ -20,4 +21,25 @@ const typedOne = new Typed("#element1", {
   ],
   typeSpeed: 80,
   loop: true,
+});
+
+const confettiBtn = document.getElementById("confetbtn");
+confettiBtn.addEventListener("click", () => {
+  const myCanvas = document.createElement("canvas");
+  myCanvas.style.position = "fixed";
+  myCanvas.style.inset = 0;
+  myCanvas.style.width = "100vw";
+  myCanvas.style.height = "100vh";
+  myCanvas.style.zIndex = -1;
+  document.body.appendChild(myCanvas);
+
+  const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true,
+  });
+
+  myConfetti({
+    particleCount: 200,
+    spread: 200,
+  });
 });
